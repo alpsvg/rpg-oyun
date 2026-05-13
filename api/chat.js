06075303a5fe743ec5d4d79a63b233e2263c2f2e
alpsvg <alp.nedret@outlook.com>
2026-05-13 05:25:29 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                model: "mistralai/mistral-7b-instruct:free",
+                qwen/qwen3-next-80b-a3b-instruct:free
                 messages: messages
             })
         });
