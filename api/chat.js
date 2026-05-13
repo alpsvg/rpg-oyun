15ce8d979a1a46afc15c2ecd66d704f19c3edc9a
alpsvg <alp.nedret@outlook.com>
2026-05-13 05:14:20 +0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -26,7 +26,7 @@ export default async function handler(req, res) {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-              model: "google/gemini-2.0-flash-exp:free",
+              model: "google/gemma-2-9b-it:free",
                 messages: messages
             })
         });
